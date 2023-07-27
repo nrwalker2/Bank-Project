fb3d4d32cf0b68149e9343b797034d0aa93be596
nrwalker2 <135749410+nrwalker2@users.noreply.github.com>
2023-07-26 19:33:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-function updateRoute(templateId) {
-  const template = document.getElementById(templateId);
+function updateRoute() {
+  const path = window.location.pathname;
+  const route = routes[path];
+
+  const template = document.getElementById(route.templateId);
   const view = template.content.cloneNode(true);
   const app = document.getElementById('app');
   app.innerHTML = '';
   app.appendChild(view);
+}
+const routes = {
+  '/login': { templateId: 'login' },
+  '/dashboard': { templateId: 'dashboard' },
+};
+function navigate(path) {
+  window.history.pushState({}, path, path);
+  updateRoute();
+}
+function updateRoute() {
+  const path = window.location.pathname;
+  const route = routes[path];
+
+  if (!route) {
+    return navigate('/login');
+  }
+function onLinkClick(event) {
+  event.preventDefault();
+  navigate(event.target.href);
 }

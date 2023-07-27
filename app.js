e31ef12d48ee1b38f663a6fb3dc08e7c3a464aa2
nrwalker2 <135749410+nrwalker2@users.noreply.github.com>
2023-07-26 19:28:44 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function updateRoute(templateId) {
+  const template = document.getElementById(templateId);
+  const view = template.content.cloneNode(true);
+  const app = document.getElementById('app');
+  app.innerHTML = '';
+  app.appendChild(view);
+}
